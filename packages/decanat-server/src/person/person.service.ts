c6c7ce0d9e7e6d27337f3b06d1ff544d4f47e4e9
Stepan Ponomarev <step.ponomarev@gmail.com>
2021-03-28 14:47:36 +0300

    tested method

--- packages/decanat-server/src/person/person.service.ts
+++ packages/decanat-server/src/person/person.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Person } from './models/person.model';
 
+//TODO: Замокано
 @Injectable()
 export class PersonService {
   private readonly cats: any[] = [];
 
   findById(id: number): Person {
-    return null;
+    return {
+      id: id,
+      firstName: 'Степан',
+      lastName: 'Пономарев',
+      patherName: 'Павлович',
+    };
   }
 }
